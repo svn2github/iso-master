68cfa732cccec20f7bf1455dfc8b41551ec3dbfc
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2005-11-11 13:20:49 +0000

    some more work on mangling
    git-svn-id: svn://littlesvr.ca/prog/isomaster@44 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- src/tests/bkMangle.c
+++ src/tests/bkMangle.c
@@ -1,33 +1,25 @@
 #include "bk.h"
+#include "bkMangle.h"
 
 #include <string.h>
 
 //!! origDir has to be sorted
 int mangleDir(Dir* origDir, Dir* newDir, int fileNameType)
 {
-    // apply filenametype rules to possible file and dir following
-    // decide whether to take from directories or files
-    // find entry name base, ext
-    // compare new name to old file name
-    // compare new name to old dir name
-      // if same, num++
-      // else, num = -1
-    // new name = base + num (opt) + ext (opt)
-    
-    FileLL* nextOrigFile;
-    char nextOrigFileName[NCHARS_FILE_ID_MAX];
-    char nextOrigFileBase[NCHARS_FILE_ID_MAX]; /* used for mangling */
-    char nextOrigFileExt[6]; /* used for mangling */
-    char prevOrigFileBase[NCHARS_FILE_ID_MAX];
-    char prevOrigFileExt[6];
-    char prevOrigFileName[NCHARS_FILE_ID_MAX];
-    
     DirLL* nextOrigDir;
     char nextOrigDirName[NCHARS_FILE_ID_MAX];
     char prevOrigDirName[NCHARS_FILE_ID_MAX];
     
-    FileLL** nextNewFile;
+    FileLL* nextOrigFile;
+    char nextOrigFileName[NCHARS_FILE_ID_MAX];
+    char nextOrigFileBase[NCHARS_FILE_ID_MAX];
+    char nextOrigFileExt[5];
+    char prevOrigFileName[NCHARS_FILE_ID_MAX];
+    /*char prevOrigFileBase[NCHARS_FILE_ID_MAX];
+    char prevOrigFileExt[6];*/
+    
     DirLL** nextNewDir;
+    FileLL** nextNewFile;
     
     bool takeDirNext; /* what to work on next, a dir or a file? */
     int fileNumber; /* longf~01.txt */
@@ -50,23 +42,23 @@ int mangleDir(Dir* origDir, Dir* newDir, int fileNameType)
         {
             takeDirNext = true;
             
-            mangleFileName(nextOrigFile->file.name, nextOrigFileName, fileNameType);
-            splitFileName(nextOrigFileName, nextOrigFileBase, nextOrigFileExt);
+            mangleDirName(nextOrigDir->dir.name, nextOrigDirName, fileNameType);
         }
         else if(nextOrigDir == NULL)
         /* no directories left */
         {
             takeDirNext = false;
             
-            mangleDirName(nextOrigDir->dir.name, nextOrigDirName);
+            mangleFileName(nextOrigFile->file.name, nextOrigFileName, fileNameType);
+            splitFileName(nextOrigFileName, nextOrigFileBase, nextOrigFileExt);
         }
         else
         /* have both a file and a directory */
         {
+            mangleDirName(nextOrigDir->dir.name, nextOrigDirName, fileNameType);
+            
             mangleFileName(nextOrigFile->file.name, nextOrigFileName, fileNameType);
             splitFileName(nextOrigFileName, nextOrigFileBase, nextOrigFileExt);
-            
-            mangleDirName(nextOrigDir->dir.name, nextOrigDirName);
             
             /* find the lesser string, that's what i want to use */
             if( strcmp(nextOrigFileName, nextOrigDirName) > 0 )
@@ -84,7 +76,7 @@ int mangleDir(Dir* origDir, Dir* newDir, int fileNameType)
         if(takeDirNext)
         {
             if( strcmp(nextOrigDirName, prevOrigDirName) == 0 ||
-                dirNameSameAsFileName(nextOrigDirName, prevOrigFileBase, prevOrigFileExt) )
+                strcmp(nextOrigDirName, prevOrigFileName) == 0 )
             {
                 // insert ~xxx in dir name 
                 
@@ -97,17 +89,15 @@ int mangleDir(Dir* origDir, Dir* newDir, int fileNameType)
             
             // add dir with new name to new list
             
-            // copy next to previous
+            strcpy(nextOrigDirName, prevOrigDirName);
             
             nextOrigDir = nextOrigDir->next;
         }
         else
         /* take file next */
         {
-            if( fileNameSameAsFileName(nextOrigFileBase, nextOrigFileExt,
-                                       prevOrigFileBase, prevOrigFileExt) ||
-                dirNameSameAsFileName(prevOrigDirName, 
-                                      nextOrigFileBase, nextOrigFileExt) )
+            if( strcmp(nextOrigFileName, prevOrigFileName) == 0 ||
+                strcmp(nextOrigFileName, prevOrigDirName) == 0 )
             {
                 // insert ~xxx in file name 
                 
@@ -120,7 +110,7 @@ int mangleDir(Dir* origDir, Dir* newDir, int fileNameType)
             
             // add file with new name to new list
             
-            // move next to previous
+            strcpy(nextOrigFileName, prevOrigFileName);
             
             nextOrigFile = nextOrigFile->next;
         }
@@ -128,4 +118,48 @@ int mangleDir(Dir* origDir, Dir* newDir, int fileNameType)
     } /* while (have a file or directory to convert) */
     
     return 1;
+}
+
+void mangleDirName(char* src, char* dest, int fileNameType)
+{
+    strcpy(src, dest);
+    
+    dest[8] = '\0';
+    
+    // get rid of bad characters
+}
+
+void mangleFileName(char* src, char* dest, int fileNameType)
+{
+    char base[NCHARS_FILE_ID_MAX];
+    char extension[5];
+    
+    splitFileName(src, base, extension);
+    
+    strncpy(dest, base, 8);
+    
+    strcat(dest, ".");
+    
+    strcat(dest, extension);
+    
+    // get rid of bad characters
+}
+
+void splitFileName(char* src, char* base, char* extension)
+{
+    int lenOrig;
+    int splitAt;
+    
+    lenOrig = strlen(src);
+    
+    /* find the dot at most four characters from the end */
+    splitAt = lenOrig - 1;
+    while(splitAt > lenOrig - 5 && splitAt >= 0 && src[splitAt] != '.')
+        splitAt--;
+    
+    /* copy base */
+    strncpy(base, src, splitAt);
+    
+    /* copy extension */
+    strncpy(extension, src + splitAt + 1, 4);
 }
--- src/tests/bkMangle.h
+++ src/tests/bkMangle.h
@@ -1,6 +1,8 @@
 #ifndef bkMangle_h
 #define bkMangle_h
 
-
+void mangleDirName(char* src, char* dest, int fileNameType);
+void mangleFileName(char* src, char* dest, int fileNameType);
+void splitFileName(char* src, char* base, char* extension);
 
 #endif
