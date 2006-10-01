01ebe5f800bc370d7a317cd590f63e76c428f3fd
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2006-10-01 20:39:22 +0000

    can now give a file.iso parameter to isomaster
    git-svn-id: svn://littlesvr.ca/prog/isomaster@144 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- src/isomaster.c
+++ src/isomaster.c
@@ -108,6 +108,9 @@ int main(int argc, char** argv)
     statusBar = gtk_statusbar_new();
     gtk_widget_show(statusBar);
     gtk_box_pack_start(GTK_BOX(mainVBox), statusBar, FALSE, FALSE, 0);
+
+    if (argv[1] != NULL)
+      openIso(argv[1]);
     
     gtk_main();
     
