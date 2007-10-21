8b2cd1dce232a63c58267a2063c17f4222b9f588
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2007-10-21 11:22:14 +0000

    win32: toolbar shows up but looks crappy
    git-svn-id: svn://littlesvr.ca/prog/isomaster@464 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- src/ISO Master VS2005/resource.h
+++ src/ISO Master VS2005/resource.h
@@ -3,6 +3,7 @@
 // Used by ISO Master VS2005.rc
 //
 #define IDR_MENU1                       101
+#define IDI_ICON1                       111
 #define ID_ASD_QWE                      40001
 #define ID_IMAGE_QUIT                   40002
 #define ID_IMAGE_OPEN                   40004
@@ -11,8 +12,8 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_COMMAND_VALUE         40010
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
--- src/ISO Master VS2005/window.cpp
+++ src/ISO Master VS2005/window.cpp
@@ -6,56 +6,52 @@
 
 extern HINSTANCE GBLhInstance;
 
+#define IDM_NEW                         1
+#define IDM_OPEN                        2
+#define IDM_SAVE                        3
+
 HWND createSimpleToolbar(HWND hWndParent)
 {
+    HWND hwnd;
+    
+    hwnd = CreateWindowEx(0, TOOLBARCLASSNAME, NULL, WS_CHILD, 
+                          0, 0, 100, 100, hWndParent, NULL, GBLhInstance, NULL);
+    if(hwnd == NULL)
+        return NULL;
+    
     const int ImageListID = 0;
     const int numButtons = 3;
     const DWORD buttonStyles = BTNS_AUTOSIZE;
     const int bitmapSize = 16;
 
-    HWND hWndToolbar = CreateWindowEx(0, L"ISOMasterTopToolbar", NULL, 
-                                      WS_CHILD | TBSTYLE_WRAPABLE,
-                                      0, 0, 0, 0,
-                                      hWndParent, NULL, GBLhInstance, NULL);
-    if (hWndToolbar == NULL)
-    {
-        return NULL;
-    }
-    
     HIMAGELIST hImageList = ImageList_Create(
-                    bitmapSize, bitmapSize,
-                    ILC_COLOR32,
-                    numButtons, 0);
-
-    // Set the image list.
-    //SendMessage(hWndToolbar, TB_SETIMAGELIST, (WPARAM)ImageListID, (LPARAM)hImageList);
-    SendMessage(hWndToolbar, TB_SETIMAGELIST, NULL, (LPARAM)hImageList);
-
-    //// Load the button images.
-    //SendMessage(hWndToolbar, TB_LOADIMAGES, (WPARAM)IDB_STD_SMALL_COLOR, 
-    //    (LPARAM)HINST_COMMCTRL);
-
-    //// Initialize button info.
-    //// IDM_NEW, IDM_OPEN, and IDM_SAVE are application-defined command constants.
-    //TBBUTTON tbButtons[numButtons] = 
-    //{
-    //    { MAKELONG(STD_FILENEW, ImageListID), IDM_NEW, TBSTATE_ENABLED, 
-    //      buttonStyles, {0}, 0, (INT_PTR)L"New" },
-    //    { MAKELONG(STD_FILEOPEN, ImageListID), IDM_OPEN, TBSTATE_ENABLED, 
-    //      buttonStyles, {0}, 0, (INT_PTR)L"Open"},
-    //    { MAKELONG(STD_FILESAVE, ImageListID), IDM_SAVE, 0, 
-    //      buttonStyles, {0}, 0, (INT_PTR)L"Save"}
-    //};
-
-    // Add buttons.
-    SendMessage(hWndToolbar, TB_BUTTONSTRUCTSIZE, 
+        bitmapSize, bitmapSize,
+        ILC_COLOR | ILC_MASK,
+        numButtons, 0);
+    
+    SendMessage(hwnd, TB_SETIMAGELIST, (WPARAM)ImageListID, 
+        (LPARAM)hImageList);
+       
+    SendMessage(hwnd, TB_LOADIMAGES, (WPARAM)IDB_STD_SMALL_COLOR, 
+        (LPARAM)HINST_COMMCTRL);
+    
+    TBBUTTON tbButtons[numButtons] = 
+    {
+        { MAKELONG(STD_FILENEW, ImageListID), IDM_NEW, TBSTATE_ENABLED, 
+          buttonStyles, {0}, 0, (INT_PTR)L"New" },
+        { MAKELONG(STD_FILEOPEN, ImageListID), IDM_OPEN, TBSTATE_ENABLED, 
+          buttonStyles, {0}, 0, (INT_PTR)L"Open"},
+        { MAKELONG(STD_FILESAVE, ImageListID), IDM_SAVE, 0, 
+          buttonStyles, {0}, 0, (INT_PTR)L"Save"}
+    };
+    
+    SendMessage(hwnd, TB_BUTTONSTRUCTSIZE, 
         (WPARAM)sizeof(TBBUTTON), 0);
-    SendMessage(hWndToolbar, TB_ADDBUTTONS, (WPARAM)numButtons, 
+    SendMessage(hwnd, TB_ADDBUTTONS, (WPARAM)numButtons, 
         (LPARAM)&tbButtons);
-
-    // Tell the toolbar to resize itself, and show it.
-    SendMessage(hWndToolbar, TB_AUTOSIZE, 0, 0); 
-    ShowWindow(hWndToolbar, TRUE);
-
-    return hWndToolbar;
+    
+    SendMessage(hwnd, TB_AUTOSIZE, 0, 0); 
+    ShowWindow(hwnd, TRUE);
+    
+    return hwnd;
 }
