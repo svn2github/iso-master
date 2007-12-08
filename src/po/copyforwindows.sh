580b309466b535cfff55e7faafd35b34652cb6d4
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2007-12-08 12:04:38 +0000

    updated a few translations for 1.3
    git-svn-id: svn://littlesvr.ca/prog/isomaster@547 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- src/po/copyforwindows.sh
+++
@@ -1,6 +0,0 @@
-for FILE in *.mo
-do
-    mkdir -p `basename $FILE .mo`/LC_MESSAGES
-    echo `basename $FILE .mo`/LC_MESSAGES/isomaster.mo
-    cp $FILE `basename $FILE .mo`/LC_MESSAGES/isomaster.mo
-done
