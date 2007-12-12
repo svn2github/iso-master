de570e7a2f6da47de6cd4efce1bd499abc53a742
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2007-12-12 21:48:16 +0000

    version is now defined in the makefile
    git-svn-id: svn://littlesvr.ca/prog/isomaster@549 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- src/about.c
+++ src/about.c
@@ -22,7 +22,7 @@ extern GtkWidget* GBLmainWindow;
 #if GTK_MINOR_VERSION >= 6
 
 static const char* 
-GBLprogramName = "ISO Master 1.2w";
+GBLprogramName = "ISO Master "VERSION;
 
 static const char* 
 GBLauthors[2] = {
